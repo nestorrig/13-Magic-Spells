7484f304e7d2788e04670a53c503c57112cdcf72
Nestor Rios Garcia <neriga10@hotmail.com>
2024-07-27 01:00:21 -0600

    feat: add preliminar loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,15 @@
 import { EVENTS, observerEmitter } from "@/Events/Events";
 import { useEffect, useState } from "react";
 
+
 export const Loader = () => {
   const [text, setText] = useState("Cargando...");
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     observerEmitter.on(EVENTS.LOADED, () => {
       setText("Cargado");
+      setLoaded(true);
       console.log("Cargado");
     });
 
@@ -15,5 +18,15 @@ export const Loader = () => {
     };
   }, []);
 
-  return <div className="absolute text-2xl font-bold">{text}</div>;
+  return (
+    <div
+      className={`absolute w-screen h-dvh text-2xl font-bold bg-slate-600 top-0 transition-all ${
+        loaded && "translate-y-full"
+      }`}
+    >
+      <div className="h-full flex justify-center items-center">
+        <p className="text-center text-white">{text}</p>
+      </div>
+    </div>
+  );
 };
